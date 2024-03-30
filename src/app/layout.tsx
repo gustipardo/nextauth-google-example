0cf8159013c6242b434cfd374aa7f928f81e6d9a
gustipardo <gusm2003@gmail.com>
2024-03-30 14:02:43 -0300

    project bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { SessionProvider } from "next-auth/react";
+import { Providers } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar/>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
